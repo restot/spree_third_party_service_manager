e126d7446cd3eb47736f5573051a792e9d1ca50b
restot <92ilya.icom@gmail.com>
2019-03-13 13:37:29 -0400

    Update 20180919124848_create_spree_pages.rb

--- db/migrate/20180919124848_create_spree_pages.rb
+++ db/migrate/20180919124848_create_spree_pages.rb
@@ -1,6 +1,6 @@
 class CreateSpreePages < SpreeExtension::Migration[4.2]
   def change
-    create_table :spree_pages do |t|
+    create_table :spree_pages_route do |t|
       t.string :name
 
       t.timestamps null: false
